ff11323c4ef747e385f606961a1487a78bd1a0ff
Nic Call <nicrudy00@gmail.com>
2019-11-01 09:49:11 -0600

    done

--- classes.js
+++ classes.js
@@ -2,18 +2,13 @@
   Once you complete a problem, refresh ./classes.html in your browser and check to see if the problem's test(s) are passing.
   Passed tests will be indicated by a green circle.
   Failed tests will be indicated by a red X.
-
   You can refresh the page at any time to re-run all the tests.
-
   Classes are a tool for building similar objects over and over again.
   They are a construct that helps your organize your code.
-
   Let's work with some employees at a company.
   You work for Widget Co. They have hundreds of employees.
 */
-
 ////////// PROBLEM 1 //////////
-
 /*
   Make a class to help us build all of the employees.
   Each employee has the following properties:
@@ -25,15 +20,22 @@
     - makeWidget
       - This returns a string equal to the employees first name + last name + the word widget
       - Example: "Dave Smith Widget"
-
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
-
 //Code Here
-
-
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name,
+    this.last_name = last_name,
+    this.email = email,
+    this.age = age
+    this.makeWidget = function(){
+      return first_name + ' ' + last_name + ' Widget';
+      
+    }
+  }
+}
 ////////// PROBLEM 2 //////////
-
 /*
   Next, make a manager for Widget Co. that extends Employee
   Each manager has all of the same properties as an employee with the following additional properties:
@@ -43,21 +45,27 @@
       - Accepts a new employee as a parameter and pushes it to their list of reports.
     - fire (index)
       - Fire removes employees from their list of reports at the given index
-
   Call your new class Manager
 */
-
 //Code Here
-
-
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age, ){
+    super(first_name, last_name, email, age)
+    this.reports = []
+  }
+  hire(employee){
+    this.reports.push(employee)
+  }
+    fire(index){
+      this.reports.splice(index, 1)
+    }
+};
 ////////// PROBLEM 3 //////////
-
 /*
   Managers for Widget Co. get promoted when they get more employees, and get a bonus when they fire employees.
   create a class ProgressiveManager that extends Manager.  A Progressive Manager has all of the same properties as a manager with the following additional properties:
     - title - default 'Not a manager'
     - bonus - default 0
-
   When employees are hired or fired, the manager's title should be updated based on the number of reports.
     0 reports : Not a manager
     1-3 reports : Barely Manager
@@ -65,28 +73,57 @@
     11-50 reports : Manager
     51-100 reports : Manager Plus
     101+ reports : Bestest Manager
-
   Everytime they fire an employee they get $100 added to their bonus.
-
   Call your new class ProgressiveManager
 */
-
 //Code Here
-
-
-
+class ProgressiveManager extends Manager {
+  constructor(first_name, last_name, email, age, reports) {
+    super(first_name, last_name, email, age, reports)
+    this.title = 'Not a manager'
+    this.bonus = 0
+  }
+  hire(employee){
+    super.hire(employee)
+    this.up()
+  }
+  fire(index){
+    super.fire(index)
+    this.bonus += 100;
+    this.up()
+  }
+  up(){
+    console.log(this.reports)
+    if(this.reports.length === 0){
+      this.title = 'Not a Manager'
+      return this.title
+    }    
+    if (this.reports.length <= 3){
+      return this.title = 'Barely Manager'
+    }
+    if(this.reports.length <= 10){
+      return this.title = 'Mostly Manager'
+    }
+    if(this.reports.length <= 50){
+      return this.title = 'Manager'
+    }
+    if(this.reports.length <= 100){
+      return this.title = 'Manager Plus'
+    }
+    if(this.reports.length >= 101){
+      return this.title = 'Bestest Manager'
+    }
+  }
+}
 ////////// PROBLEM 4 - Black Diamond //////////
-
 /*
   Widget Co has a factory that makes widgets.
   Factories have Machines.
-
   Make a Machine class that takes in no parameters
   A Machine has the following properties:
     - widgets_made_count - default 0
     - wear_and_tear_count - default 0
     - needs_reboot - default false
-
   A Machine has the following methods:
     - makeWidgets
         - This function takes in a number and increases widgets_made_count by that amount
@@ -97,7 +134,5 @@
         - This function returns an anonymous function that is called when the machine is done rebooting
         - The anonymous function should decrease wear_and_tear_count by 10, and set needs_reboot to false
 */
-
 //Code Here
-
 
